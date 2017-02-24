c68232f6f90bda18049dfe64a31210330d56dde6
Maku <makucross@gmail.com>
2017-02-23 19:17:31 -0500

    The recording box now goes away when it's not in use

--- voice/vGUI.py
+++ voice/vGUI.py
@@ -16,30 +16,27 @@ class VGUI:
 		
 		master.title("Intercom")
 		
-		self.recData = 3 #This will be actual voice later
-		self.recDataEntry = Entry(master)
-		self.recDataEntry.pack()
-		
 		labelText = "The purpose of this is for recording messages to be sent to other computers on the network."
 		self.label = Label(master, text = labelText)
 		self.label.pack()
 		self.record_button = Button(master, text="Record Message",command=self.recordPress)
 		self.record_button.pack()
-		self.close_button = Button(master, text="Close Window",command=master.quit)
-		self.close_button.pack()
+		
+		self.recDataEntry = Entry(master)
+		
 		mainloop()
 	
 	def recordPress(self):
 		wasRecording = (self.record_button["text"] == "Stop Recording") #There must be a better way of doing this
 		if wasRecording:
 			print "Normally, this would stop the recording."
-			#STOP RECORDING
 			self.sendData(self.recDataEntry.get())#This will send what's in the rec data box thing
+			self.recDataEntry.pack_forget()
 			self.record_button["text"] = "Record Message"
 		else:
 			print "Normally, this would start a recording."
+			self.recDataEntry.pack()
 			self.recDataEntry.delete(0,END)
-			#START RECORDING
 			self.record_button["text"] = "Stop Recording"
 	def sendData(self, data):
 		print "The user ",self.getName()," has send the data: ",data
