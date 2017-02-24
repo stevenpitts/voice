df0bf98e2171adb21516fe949c3253aa2fb5351c
Maku <makucross@gmail.com>
2017-02-23 20:26:29 -0500

    Improved separation of audio methods

---
+++ sound/audioRecorder.py
@@ -0,0 +1,8 @@
+class AudioRecorder:
+	def __init__(self, gui):
+		#infoDict should have the audio device or some way to find it
+		self.vGUI = gui
+	def recordAudio(self):
+		#somehow record audio here
+		data = "This is what was recorded"
+		return data
--- voice/vGUI.py
+++ voice/vGUI.py
@@ -2,6 +2,7 @@
 import Tkinter
 from Tkinter import *
 from sound.sound import Sound
+from sound.audioRecorder import AudioRecorder
 """
 Resources:
 http://python-textbok.readthedocs.io/en/1.0/Introduction_to_GUI_Programming.html'
@@ -14,30 +15,23 @@ class VGUI:
 		
 		self.master = master
 		self.infoDict = infoDict
+		self.audioRecorder = AudioRecorder(self)
 		
 		master.title("Intercom")
 		
-		labelText = "The purpose of this is for recording messages to be sent to other computers on the network."
-		self.label = Label(master, text = labelText)
-		self.label.pack()
+		Label(master, text = "Send your message to others on the network!\n\n").pack()
 		self.record_button = Button(master, text="Record Message",command=self.recordPress)
 		self.record_button.pack()
-		
-		self.recDataEntry = Entry(master)
 		
 		mainloop()
 	
 	def recordPress(self):
 		wasRecording = (self.record_button["text"] == "Stop Recording") #There must be a better way of doing this
 		if wasRecording:
-			print "Normally, this would stop the recording."
-			self.sendData(self.recDataEntry.get())#This will send what's in the rec data box thing
-			self.recDataEntry.pack_forget()
+			data = self.audioRecorder.recordAudio()
+			self.sendData(data)
 			self.record_button["text"] = "Record Message"
 		else:
-			print "Normally, this would start a recording."
-			self.recDataEntry.pack()
-			self.recDataEntry.delete(0,END)
 			self.record_button["text"] = "Stop Recording"
 	def sendData(self, data):
 		newSound = Sound(data,self.getName())
