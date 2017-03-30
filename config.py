965a22a3df30aebd5e3ba09c5d5e1ee4db10e377
Maku <makucross@gmail.com>
2017-03-30 12:18:01 -0400

    Fixing last push

---
+++ config.py
@@ -0,0 +1,11 @@
+import pyaudio
+makuServerIP = '10.200.23.5'
+makuChunk = 1024
+makuFormat = pyaudio.paInt16
+makuChannels = 2
+makuRate = 44100
+makuRecordSeconds = 5
+makuOutputFilename = "makuOutput.wav"
+makuPort = 12345
+makuMaxClients = 5
+makuRecVal = 1024
---
+++ main.py
@@ -0,0 +1,7 @@
+import vServer
+import vClient
+from vServer import *
+from vClient import *
+import thread
+import Tkinter
+from Tkinter import *
